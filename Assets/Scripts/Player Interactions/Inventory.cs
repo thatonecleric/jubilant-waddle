621c7242167254a7f135c72911c07d1b1613ecf1
Mai Blom <mai.blommen@gmail.com>
2023-10-01 05:43:55 +0200

    shitty outline works

--- Assets/Scripts/Player Interactions/Inventory.cs
+++ Assets/Scripts/Player Interactions/Inventory.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
+    [SerializeField] public Image KeyIcon;
     public bool HasKey = false;
 
     public void Update()
